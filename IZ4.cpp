467a53b77d91c2b62d1c0bfc5500f2d337138e75
Czernee <tehnikvalerij@gmail.com>
2023-05-21 15:55:00 +0300

    probably last version

--- IZ4.cpp
+++ IZ4.cpp
@@ -1,16 +1,21 @@
-﻿#include <iostream>
+﻿// ЮФУ ИКТИБ МОП ЭВМ
+// Программирование и Основы Теории Алгоритмов 2
+// Индивидуальное задание № 4 Проектирование и реализация нормального алгоритма Маркова
+// КТбо1-7 Черноусов Даниил Владимирович 
+
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <windows.h>
 
 using namespace std;
 
-struct Machine
+typedef struct Machine
     {
     string whatToReplace;
     string replaceTo;
     bool ifEnd;
-    };
+    } Machine;
 
 int init(Machine* ptr)
     {
@@ -31,60 +36,73 @@ int init(Machine* ptr)
     return cnt;
     }
 
-string transition(Machine* ptr, string str, int cntStates, bool& isEnd)
+string transition(Machine* ptr, string str, int cntStates, bool* isEnd)
     {
     string s = str;
     HANDLE hConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
     for (int i = 0; i < cntStates; i++)
         {
-        size_t foundPos = str.find(ptr[i].whatToReplace);
-        if (foundPos != string::npos)
+        if (ptr[i].whatToReplace == "_")
             {
-            string replacedStr = str;
+            str = ptr[i].replaceTo + str;
+            *isEnd = ptr[i].ifEnd;
+
+            cout << "(" << i + 1 << ") " << s << " -> ";
+            for (int k = 0; k < str.size(); k++)
+                {
+                SetConsoleTextAttribute(hConsoleHandle, (k < ptr[i].replaceTo.size()) ? 4 : 15);
+                cout << str[k];
+                }
+            SetConsoleTextAttribute(hConsoleHandle, 15);
+
+            cout << endl;
+            return str;
+            }
+
+        size_t replacePos = str.find(ptr[i].whatToReplace);
+        if (replacePos != string::npos)
+            {
             if (ptr[i].replaceTo == "_")
                 {
-                replacedStr.replace(foundPos, ptr[i].whatToReplace.size(), "");
+                str.erase(replacePos, ptr[i].whatToReplace.size());
                 }
             else
                 {
-                replacedStr.replace(foundPos, ptr[i].whatToReplace.size(), ptr[i].replaceTo);
+                str.replace(replacePos, ptr[i].whatToReplace.size(), ptr[i].replaceTo);
                 }
+
+            *isEnd = ptr[i].ifEnd;
+
             cout << "(" << i + 1 << ") ";
             for (int k = 0; k < s.size(); k++)
                 {
-                if (k >= foundPos && k < foundPos + replacedStr.size())
-                    {
-                    SetConsoleTextAttribute(hConsoleHandle, 4);
-                    cout << s[k];
-                    }
-                else
-                    {
-                    SetConsoleTextAttribute(hConsoleHandle, 15);
-                    cout << s[k];
-                    }
+                SetConsoleTextAttribute(hConsoleHandle, (k >= replacePos && k < replacePos + ptr[i].whatToReplace.size()) ? 4 : 15);
+                cout << s[k];
                 }
             cout << " -> ";
-            for (int k = 0; k < replacedStr.size(); k++)
+            for (int k = 0; k < str.size(); k++)
                 {
-                SetConsoleTextAttribute(hConsoleHandle, 4);
-                cout << replacedStr[k];
+                SetConsoleTextAttribute(hConsoleHandle, (k >= replacePos && k < replacePos + ptr[i].replaceTo.size()) ? 4 : 15);
+                cout << str[k];
                 }
             SetConsoleTextAttribute(hConsoleHandle, 15);
+
             cout << endl;
-            str = replacedStr;
-            isEnd = ptr[i].ifEnd;
             return str;
             }
         }
-    isEnd = false;
+
+    *isEnd = false;
     return str;
     }
 
-bool isValidInput(const string& input)
+
+bool isValidInput(string input)
     {
     for (char c : input)
         {
-        if (c != '1' && c != '*')
+        if (!strchr("1*", c))
             {
             return false;
             }
@@ -97,17 +115,20 @@ int main()
     setlocale(LC_ALL, "Russian");
     string str;
     char choice;
-    Machine ptr[32];
+    bool flag;
+    Machine* ptr;
+    ptr = new Machine[32];
     int cntStates = init(ptr);
     do
         {
         cout << "Введите исходное слово: ";
         cin >> str;
-        bool flag = isValidInput(str);
-        if (flag)
+        flag = isValidInput(str);
+        if (flag == 1)
             {
-            bool isEnd = true;
-            while (isEnd)
+            flag = true;
+            bool* isEnd = &flag;
+            while (*isEnd == 1)
                 {
                 str = transition(ptr, str, cntStates, isEnd);
                 }
@@ -124,3 +145,4 @@ int main()
 
         return 0;
     }
+
